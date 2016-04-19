8dea49b34839dc269097d7bc35067ef620ca595a
AvramPop <dani_pop08@yahoo.com>
2016-04-19 11:54:07 +0300

    updates 19.4

--- app/src/main/java/com/bdbrainiacs/justjava/MainActivity.java
+++ app/src/main/java/com/bdbrainiacs/justjava/MainActivity.java
@@ -2,10 +2,10 @@ package com.bdbrainiacs.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -24,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        //display(1);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.weep_cream_checkbox);
+        boolean mHasWhippedCream =whippedCreamCheckBox.isChecked();
+
+        int price = calculatePrice();
+        String message = createOrderSummary(price, mHasWhippedCream);
+        displaySummary(message);
+        Log.v("MainActivity", message);
     }
 
     /**
@@ -39,22 +45,29 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number) + "\nThank you!");
+    private void displaySummary(String text) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(text);
     }
 
     public void increment(View view) {
         quantity++;
         display(quantity);
-        displayPrice(quantity * 5);
     }
 
     public void decrement(View view) {
-        if(quantity > 0) {
+        if (quantity > 0) {
             quantity--;
         }
         display(quantity);
-        displayPrice(quantity * 5);
+    }
+
+    private int calculatePrice(){
+        return quantity * 5;
+    }
+
+    private String createOrderSummary(int price, boolean hasWhippedCream){
+        String message = "Name: Dani" + "\n" + (hasWhippedCream ? "With Whipped Cream" : "Without Whipped Cream") + "\nQuantity:" + quantity + "\nTotal: $" + price + "\nThank You!";
+        return message;
     }
 }
